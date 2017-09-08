433ec44a5a8f6d577fb54b0550cc773d591737a1
Roberto Linares <blendboy@gmail.com>
2017-09-08 01:08:32 -0600

    Adición de clases base.
    Adición de las clases base del proyecto para almacenar las tareas y categorías además de la clase Repository que devolverá la información de los datos almacenados en Dropbox.

---
+++ src/main/java/org/robertux/data/TasksRepository.java
@@ -0,0 +1,25 @@
+package org.robertux.data;
+
+import org.robertux.data.model.Category;
+import org.robertux.data.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by robertux on 9/8/17.
+ */
+public class TasksRepository {
+
+    public List<Category> getCategories() {
+        List<Category> categories = new ArrayList<Category>(0);
+
+        return categories;
+    }
+
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<Task>(0);
+
+        return tasks;
+    }
+}
