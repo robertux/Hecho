76c33af02f14bf4ad818ca741e6470b7c98e0520
Roberto Linares <blendboy@gmail.com>
2017-12-19 23:00:37 -0600

    Implementación del método getSyncUrl para la clase GDriveProvider

--- src/main/java/org/robertux/data/syncProviders/GDriveProvider.java
+++ src/main/java/org/robertux/data/syncProviders/GDriveProvider.java
@@ -1,11 +1,13 @@
 package org.robertux.data.syncProviders;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleBrowserClientRequestUrl;
 import org.robertux.data.model.JsonResponse;
 import spark.Request;
 
 import javax.servlet.http.HttpSession;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 /**
  * Created by robertux on 11/7/17.
@@ -14,6 +16,8 @@ public class GDriveProvider extends CloudSyncProvider {
     public static final String PROVIDER_NAME = "gdrive";
     public static final String PROVIDER_DISPLAY_NAME = "Google Drive";
     public static final String LOGO_URL = "/img/providers/Drive Lockup - with gray Google.png";
+
+    private static final String REDIRECT_URL = "https://hecho.herokuapp.com/api/providers/gdrive/auth";
 
     @Override
     public String getName() {
@@ -37,7 +41,8 @@ public class GDriveProvider extends CloudSyncProvider {
 
     @Override
     public String getSyncUrl(HttpSession session, CloudSyncSessionData sessionData) {
-        return "";
+        return new GoogleBrowserClientRequestUrl("696464267146-tik0vr1bnql2hdqppo0k73ennnj4819o.apps.googleusercontent.com",
+                REDIRECT_URL, Arrays.asList("", "")).build();
     }
 
     @Override
@@ -47,11 +52,17 @@ public class GDriveProvider extends CloudSyncProvider {
 
     @Override
     public JsonResponse save(InputStream dbContent, String dbName, CloudSyncSessionData sessionData) {
-        return null;
+        if (sessionData == null || !sessionData.isInSync()) {
+            return JsonResponse.fromCode(1205);
+        }
+        return new JsonResponse(0, "Datos guardados exitosamente");
     }
 
     @Override
     public JsonResponse load(OutputStream dbContent, String dbName, CloudSyncSessionData sessionData) {
-        return null;
+        if (sessionData == null || !sessionData.isInSync()) {
+            return JsonResponse.fromCode(1205);
+        }
+        return new JsonResponse(0, "Datos cargados exitosamente");
     }
 }
